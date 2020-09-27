9a9966a52b9222745caefd75df54cee8b72bc36c
nickd <nickdiaz58@gmail.com>
2020-09-26 21:56:01 -0400

    Created login and signup forms, tested error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,84 @@
 class UsersController < ApplicationController
-  
 
-
-
-  ################
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  get "/users/signup" do
+    erb :"/users/signup.html"
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  post "/users/signup" do
+    user = User.new(:username => params[:username], :password => params[:password])
+ 
+    if user.save && !user.username.empty?
+      redirect "/users/login" #<<-----
+    else
+      redirect "/signup-failure"
+    end
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
+  get "/users/login" do
+    erb :"/users/login.html"
   end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
+  post "/users/login" do
+    user = User.find_by(:username => params[:username])
+ 
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account" #<<-----
+    else
+      redirect "/login-failure"
+    end
   end
 
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
+  get "/signup-failure" do
+    erb :"/users/signup-failure.html"
   end
 
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
+  get "/login-failure" do
+    erb :"/users/login-failure.html"
   end
 
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
+
+
+
+
+#   ################
+#   ################
+
+
+
+
+#   # GET: /users
+#   get "/users" do
+#     erb :"/users/index.html"
+#   end
+
+#   # GET: /users/new
+#   get "/users/new" do
+#     erb :"/users/new.html"
+#   end
+
+#   # POST: /users
+#   post "/users" do
+#     redirect "/users"
+#   end
+
+#   # GET: /users/5
+#   get "/users/:id" do
+#     erb :"/users/show.html"
+#   end
+
+#   # GET: /users/5/edit
+#   get "/users/:id/edit" do
+#     erb :"/users/edit.html"
+#   end
+
+#   # PATCH: /users/5
+#   patch "/users/:id" do
+#     redirect "/users/:id"
+#   end
+
+#   # DELETE: /users/5/delete
+#   delete "/users/:id/delete" do
+#     redirect "/users"
+#   end
 end
